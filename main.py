8372921789092030395f1611063b0103403796c6
XXXayubend <khadhraoui0202@gmail.com>
2025-04-04 15:37:23 +0200

    data validation completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipline
+from textSummarizer.pipeline.stage_02_datavalidation import DataValidationTrainingPipline
 from textSummarizer.logging import logger
 
 
@@ -10,4 +11,17 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
-    raise e     
+    raise e  
+
+
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} <<<<<<")
+    data_validation = DataValidationTrainingPipline()
+    data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e 
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,6 +1,7 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
-from textSummarizer.entity import (DataIngestionConfig)
+from textSummarizer.entity import (DataIngestionConfig,
+                                   DataValidationConfig)
 
 
 class ConfigurationManager:
@@ -12,7 +13,7 @@ class ConfigurationManager:
             self.config = read_yaml(config_filepath)
             self.params = read_yaml(params_filepath)
 
-            create_directories([self.config.artfifacts_root])
+            create_directories([self.config.artifacts_root])
 
 
 
@@ -29,3 +30,17 @@ class ConfigurationManager:
             )
 
             return data_ingestion_config
+    
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+            config = self.config.data_validation
+
+            create_directories([config.root_dir])
+
+            data_validation_config = DataValidationConfig(
+                   root_dir=config.root_dir,
+                   STATUS_FILE=config.STATUS_FILE,
+                   ALL_REQUIRED_FILE=config.ALL_REQUIRED_FILE,
+            )
+
+            return data_validation_config
